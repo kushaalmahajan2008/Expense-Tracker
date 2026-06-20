56c77b98b66194b4c7a0d19349a61def3c4cacff
Kushaal Mahajan <kushaal.mahajan.2008@gmail.com>
2026-06-19 23:19:26 -0700

    Add database initializing file

---
+++ init__db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+expense_table="""CREATE TABLE IF NOT EXISTS Expenses
+    (expnese_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    expense_date TEXT NOT NULL,
+    expense_amount REAL NOT NULL,
+    expense_category TEXT NOT NULL,
+    expense_tag TEXT NOT NULL,
+    expense_description TEXT )"""
+
+tag_table="""CREATE TABLE IF NOT EXISTS TAGS
+    (Tags Text NOT NULL)"""
+
+
+with sqlite3.Connection("expense.db") as conn:
+    conn.execute(expense_table)
+    conn.execute(tag_table)
